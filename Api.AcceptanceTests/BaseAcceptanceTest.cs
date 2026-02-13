7ea29f8aa4abba304ab0acac0475d6c1851d027d
tommymapp <tommy.mapp@hotmail.com>
2026-02-13 16:21:58 +0000

    Add BaseAcceptanceTest

--- Api.AcceptanceTests/BaseAcceptanceTest.cs
+++ Api.AcceptanceTests/BaseAcceptanceTest.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace Api.AcceptanceTests;
 
-public class BaseAcceptanceTest
+public class BaseAcceptanceTest : IDisposable
 {
-    
+    protected readonly HttpClient Client;
+    protected readonly WebApplicationFactory<Program> Factory;
+
+    protected BaseAcceptanceTest()
+    {
+        Factory = new WebApplicationFactory<Program>();
+        Client = Factory.CreateClient();
+    }
+
+    public void Dispose()
+    {
+        Client.Dispose();
+        Factory.Dispose();
+    }
 }
