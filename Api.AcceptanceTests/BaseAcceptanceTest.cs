865269df7ba0e4b9bac2566aefc4f3ed114be5cc
tommymapp <tommy.mapp@hotmail.com>
2026-02-16 13:19:23 +0000

    Add mysql docker container for acceptance testing via Testingcontainers package/

--- Api.AcceptanceTests/BaseAcceptanceTest.cs
+++ Api.AcceptanceTests/BaseAcceptanceTest.cs
@@ -1,23 +1,45 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testcontainers.MySql;
 
 namespace Api.AcceptanceTests;
 
-public class BaseAcceptanceTest : IDisposable
+[TestClass]
+public class BaseAcceptanceTest : IAsyncDisposable
 {
+    protected string? MySqlConnectionString { get; }
+    private static MySqlContainer mysqlContainer = new MySqlBuilder()
+        .WithImage("mysql:8.0")
+        .WithDatabase("WatchDb")
+        .WithUsername("root")
+        .WithPassword("password123")
+        .Build();
+    
     protected readonly HttpClient Client;
-    protected readonly WebApplicationFactory<Program> Factory;
-
+    readonly WebApplicationFactory<Program> factory;
+    
+    [AssemblyInitialize]
+    public static async Task AssemblyInitialize(TestContext context)
+    {
+        await mysqlContainer.StartAsync();
+    }
+    
     protected BaseAcceptanceTest()
     {
-        Factory = new WebApplicationFactory<Program>();
-        Client = Factory.CreateClient();
+        MySqlConnectionString = mysqlContainer.GetConnectionString();
+
+        factory = new WebApplicationFactory<Program>();
+        Client = factory.CreateClient();
     }
 
-    public void Dispose()
+    public async ValueTask DisposeAsync()
     {
+        await mysqlContainer.DisposeAsync();
         Client.Dispose();
-        Factory.Dispose();
+        await factory.DisposeAsync();
     }
 }
