eecfb487346b76c9faa79f1be0689b0b858f2554
tommymapp <tommy.mapp@hotmail.com>
2026-02-16 10:51:56 +0000

    Add body validation for creating a new watch request

--- Api.AcceptanceTests/Features/CreateWatchTests.cs
+++ Api.AcceptanceTests/Features/CreateWatchTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -21,5 +22,24 @@ public class CreateWatchTests : BaseAcceptanceTest
         var response = await Client.PostAsJsonAsync("/api/watch",  request);
         
         Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [TestMethod]
+    // Url
+    [DataRow("Missing URL", "", 100, "test@test.com")]
+    [DataRow("Invalid URL Format", "not-a-url", 100, "test@test.com")]
+    // Target price
+    [DataRow("Free Price", "https://amazon.co.uk/Keychron-K2-HE-Wireless-Mechanical/dp/B0F63BK4ZB", 0, "test@test.com")]
+    [DataRow("Negative Price", "https://amazon.co.uk/Keychron-K2-HE-Wireless-Mechanical/dp/B0F63BK4ZB", -1, "test@test.com")]
+    // Email
+    [DataRow("Invalid Email", "https://amazon.co.uk/Keychron-K2-HE-Wireless-Mechanical/dp/B0F63BK4ZB", 100, "bad-email")]
+    [DataRow("Missing Email", "https://amazon.co.uk/Keychron-K2-HE-Wireless-Mechanical/dp/B0F63BK4ZB", 100, "")]
+    public async Task Given_Invalid_Payload_Returns_400(string scenario, string url, double price, string email)
+    {
+        var request = new { Url = url, TargetPrice = price, Email = email };
+     
+        var response = await Client.PostAsJsonAsync("/api/watch", request);
+     
+        Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode, $"Failed on scenario: {scenario}");
     }
 }
