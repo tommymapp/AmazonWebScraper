539e9b70e2d187fa222284f43a3294151580c198
tommymapp <tommy.mapp@hotmail.com>
2026-02-17 13:21:13 +0000

    Fix incorrect testing price HTML

--- Api.UnitTests/AmazonPriceParser.Tests.cs
+++ Api.UnitTests/AmazonPriceParser.Tests.cs
@@ -14,10 +14,10 @@ public class AmazonPriceParser_Tests
         var price = priceParser.GetPrice(html);
         Assert.AreEqual((decimal)expectedPrice, price);
     }
-    
+
     [TestMethod]
-    [DataRow("<span class=\"a-price-whole\">89<span class=\"a-price-decimal\">56</span></span>", 89.56)]
-    [DataRow("<span class=\"a-price-whole\">154<span class=\"a-price-decimal\">18</span></span>", 154.18)]
+    [DataRow("<span class=\"a-price-whole\">89<span class=\"a-price-decimal\">.</span></span><span class=\"a-price-fraction\">56</span>", 89.56)]
+    [DataRow("<span class=\"a-price-whole\">154<span class=\"a-price-decimal\">.</span></span><span class=\"a-price-fraction\">18</span>", 154.18)]
     public void Given_AmazonHTML_Then_ExtractsCorrectPriceWithDecimalNumbers(string html, double expectedPrice)
     {
         var priceParser = new AmazonPriceParser();
